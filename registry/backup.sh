d60bb10b7235120e0c0a22423b84bece9db4db3d
Ivo Woltring <Ivo@IvoNet.nl>
2019-02-28 16:12:17 +0100

    added cleanup and added volumes to git ignore

--- registry/backup.sh
+++ registry/backup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-docker run -it --rm -v registry_registry_volume:/volume -v $(pwd)/backup:/backup localhost:5000/alpine \
-       tar -cjfv /backup/registry.tar.bz2 -C /volume ./
+mkdir ./backup
+docker run -it --name regbak --rm -v registry_registry_volume:/volume -v $(pwd)/backup:/backup localhost:5000/alpine \
+       tar -cjf /backup/registry.tar.bz2 -C /volume ./
 
